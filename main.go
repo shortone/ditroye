96f514558d40494dc43c785ad50d90c3e20fd2df
shortone <shortone@dictive.ch>
2012-09-08 13:37:05 +0200

    Properly handle static page and backone.js example

--- main.go
+++ main.go
@@ -13,34 +13,40 @@ func retrievePlayerProfile(w http.ResponseWriter, req *http.Request) {
 
 	playerProfile, err := GetPlayerProfile("eu", battleTag)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Unable to retrieve player profile", 500)
+		return
 	}
 
 	w.Header().Set("Content-type", "application/json")
 	if err = json.NewEncoder(w).Encode(playerProfile); err != nil {
-		log.Fatal(err)
+		http.Error(w, "Unable to build response", 500)
 	}
 }
 
 func retrieveHeroProfile(w http.ResponseWriter, req *http.Request) {
 	battleTag := req.URL.Query().Get(":battleTag")
-	heroId, _ := strconv.Atoi(req.URL.Query().Get(":heroId"))
+	heroId, err := strconv.Atoi(req.URL.Query().Get(":heroId"))
+	if err != nil {
+		http.Error(w, "Invalid hero id", 500)
+		return
+	}
 
 	heroProfile, err := GetHeroProfile("eu", battleTag, heroId)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Unable to retrieve hero profile", 500)
+		return
 	}
 
 	w.Header().Set("Content-type", "application/json")
 	if err = json.NewEncoder(w).Encode(heroProfile); err != nil {
-		log.Fatal(err)
+		http.Error(w, "Unable to build response", 500)
 	}
 }
 
 func main() {
 	m := pat.New()
 
-	//	m.Get("/", http.FileServer(http.Dir("/static")))
+	m.Get("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	m.Get("/profile/:battleTag/", http.HandlerFunc(retrievePlayerProfile))
 	m.Get("/profile/:battleTag/hero/:heroId/", http.HandlerFunc(retrieveHeroProfile))
